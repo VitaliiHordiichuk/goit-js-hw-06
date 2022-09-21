6825eaba3a3b8ec6c0dc717016f2bd1580e09bd6
Vialii Hordiichuk <vgordiychuk5@gmail.com>
2022-09-21 13:17:34 +0300

    правка заглавніх букв

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,17 @@
-const form = document.querySelector('.login-form');
+const form = document.querySelector(".login-form");
 
-form.addEventListener('submit', onHandleSubmit);
+form.addEventListener("submit", onHandleSubmit);
 
 function onHandleSubmit(event) {
-     event.preventDefault();
-     const {
-          elements: { email, password },
-     } = event.currentTarget;
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
-     if (email.value === '' || password.value === '') {
-          return alert('Please fill in all the fields!');
-     }
-     const userDetails = { email: email.value, Password: password.value };
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const userDetails = { email: email.value, password: password.value };
 
-     console.log(userDetails);
-     event.currentTarget.reset();
+  event.currentTarget.reset();
 }
